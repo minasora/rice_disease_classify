c952262102f952d327baaff4eef8dff210335a65
liboxiao <43931687+minasora@users.noreply.github.com>
2023-12-21 13:13:04 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,18 +2,28 @@ self.addEventListener('install', (event) => {
     console.log('Service Worker 安装');
 });
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.endsWith('model.onnx')) {
-        event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request).then((response) => {
-                    let responseToCache = response.clone();
-                    caches.open('model-cache').then((cache) => {
-                        cache.put(event.request, responseToCache);
-                    });
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(function(response) {
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
                     return response;
                 });
             })
-        );
-    }
+    );
 });
+
